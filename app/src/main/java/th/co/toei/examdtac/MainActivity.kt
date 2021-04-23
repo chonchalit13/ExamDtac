81c17bada39bfc79cb62d532486ea81ed1403f28
TOEI <chonchalit.t@appman.co.th>
2021-04-23 15:39:49 +0700

    init project

---
+++ app/src/main/java/th/co/toei/examdtac/MainActivity.kt
@@ -0,0 +1,11 @@
+package th.co.toei.examdtac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
